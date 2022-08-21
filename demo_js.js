a63c01f5bceb8fefce50c77895bac437688d4dc5
VSthebusybee <vsharma0566@gmail.com>
2022-08-21 21:03:31 +0530

    all set

---
+++ demo_js.js
@@ -0,0 +1,67 @@
+//let msg;
+/*msg="Hello, Wolrd!";
+alert(msg);
+
+let a=10;
+let b=7*a;
+
+console.log(b);
+
+max=57;
+actual=max-13;
+percentage=actual/max;
+
+const ques='What is your name?';
+const names='Vishal';
+const join=`${ques}${names}`;
+console.log(join);
+
+const button = document.querySelector('button');
+
+function greet() {
+  const name = prompt('What is your name?');
+  alert(`Hello ${name}, nice to see you!`);
+}
+
+button.addEventListener('click', greet);
+
+function add7(a,b)
+{
+  return a+b;
+}
+b=7;
+a=8;
+let sum=add7(a,b);
+alert(sum);
+
+
+function multiply(a,b)
+{
+    return a*b;
+}
+a=prompt("ENter 1st number");
+b=prompt("ENter 2nd number");
+let sum=multiply(a,b);
+alert(sum);
+
+
+
+
+function capitalize()
+{
+  const text=prompt("Enter any number:");
+  const result=text.toUpperCase();
+  alert(result);
+
+}
+capitalize();
+*/
+
+function lastletter()
+{
+  const text=prompt("Enter any number to find last number");
+  const result=text.slice(-1);
+  alert(result);
+}
+
+lastletter();
